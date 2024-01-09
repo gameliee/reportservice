585e686b4f3931cbf6c6050382ae643f8b2a1df9
dat <datnt527@viettel.com.vn>
2024-01-09 14:25:32 +0700

    test: add staffs collection

--- tests/mongo/import.sh
+++ tests/mongo/import.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
 mongoimport --host localhost --username foo --password password --authenticationDatabase admin --db FaceID --collection BodyFaceName --type json --file /docker-entrypoint-initdb.d/data.json
+mongoimport --host localhost --username foo --password password --authenticationDatabase admin --db FaceID --collection staffs --type json --file /docker-entrypoint-initdb.d/staff.json
